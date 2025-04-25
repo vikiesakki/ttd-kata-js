95e5481063ae701f7ada5c91046f9cd61bcd7352
vignesh esakki muthu <vemcse@gmail.com>
2025-04-25 23:01:38 +0530

    test case file added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div>test file</div>
+    <div style={{maxWidth: "1280px",margin: "auto",left:0,right:0,top:0,position:"absolute",padding: "2rem",textAlign: "center"}}>
+      <h1>Calculate Addition</h1>
+      <div className="card">
+        <input data-testid="inputValue" id="inputValue" type="text" onChange={(e)=>setCount(e.target.value)}/>
+        <button onClick={setAddition}>Add</button>
+        <div data-testid="result">{finalAdd}</div>
+        <div data-testid="errorMsg">{errorMessage}</div>
+      </div>
+    </div>
   )
 }
 
