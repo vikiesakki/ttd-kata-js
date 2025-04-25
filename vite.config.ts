acec200b2c8f8d7040bdeed5524f23c0e7d58c35
vignesh esakki muthu <vemcse@gmail.com>
2025-04-25 23:49:59 +0530

    build folder name changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ttd-kata-js/',
   plugins: [react()],
   build: {
     outDir: 'build', // <-- this changes 'dist' to 'build'
